efa66221e0c5a2a9360de3f50588c96f590152a5
Asher Beck <beckasher7@gmail.com>
2026-06-18 13:13:50 -0700

    Fixed cmake module incompatibility (no need to load) and changed Hyak Klone cpu build to use newer ompi/gcc instead of ompi 4.1.4

--- machines/configure.hyak.cpu.sh
+++ machines/configure.hyak.cpu.sh
@@ -1,6 +1,7 @@
-module load cmake ompi/4.1.4
+module purge
+module load gcc/15.2.0 ompi/4.1.6 
 
 # Hyak-specific copy of the local Linux CPU configuration.
 # Keep machines/configure.linux.cpu.sh aligned with upstream defaults.
 : "${PREFIX:=/mmfs1/gscratch/stf/abeck502/gkylsoft}"
-./configure CC=cc --prefix=$PREFIX --use-mpi=yes --mpi-inc=/sw/ompi/4.1.4/include --mpi-lib=/sw/ompi/4.1.4/lib --use-lua=yes --app=moments
+./configure CC=cc --prefix=$PREFIX --use-mpi=yes --mpi-inc=/sw/ompi/4.1.6/include --mpi-lib=/sw/ompi/4.1.6/lib --use-lua=yes --app=moments
